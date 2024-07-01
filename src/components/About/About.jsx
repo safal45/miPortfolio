0b99ace9455534318a5787d1a6dea11e2e6584c9
safal45 <ptksafalpathak@gmail.com>
2024-07-01 21:23:04 +0530

    update responsiveness

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,86 +3,97 @@ import LanguageComponent from "../Skills/Skills";
 
 function About() {
   const languageData = [
-    { name: " ", logo: "C.png" },
-    { name: " ", logo: "Cpp.png" },
-    { name: " ", logo: "Python.png" },
-    { name: " ", logo: "JavaScript.png" },
-    { name: " ", logo: "HTML5.png" },
-    { name: " ", logo: "CSS3.png" },
-    { name: " ", logo: "Git.png" },
-    { name: " ", logo: "giii.png" },
-    { name: " ", logo: "Tailwind CSS.png" },
-    { name: " ", logo: "Bootstrap.png" },
-    { name: " ", logo: "PyCharm.png" },
-    { name: " ", logo: "vsc.png" },
-    { name: " ", logo: "logo192.png" },
-    { name: " ", logo: "icons8-flask-600.png" },
-    { name: " ", logo: "MySQL.png" },
-    { name: " ", logo: "SQLite.png" },
+    { name: "C", logo: "C.png" },
+    { name: "C++", logo: "Cpp.png" },
+    { name: "Python", logo: "Python.png" },
+    { name: "JavaScript", logo: "JavaScript.png" },
+    { name: "HTML5", logo: "HTML5.png" },
+    { name: "CSS3", logo: "CSS3.png" },
+    { name: "Git", logo: "Git.png" },
+    { name: "GitHub", logo: "giii.png" },
+    { name: "Tailwind CSS", logo: "Tailwind CSS.png" },
+    { name: "Bootstrap", logo: "Bootstrap.png" },
+    { name: "PyCharm", logo: "PyCharm.png" },
+    { name: "VSCode", logo: "vsc.png" },
+    { name: "React", logo: "logo192.png" },
+    { name: "Flask", logo: "icons8-flask-600.png" },
+    { name: "MySQL", logo: "MySQL.png" },
+    { name: "SQLite", logo: "SQLite.png" },
   ];
 
   return (
-    <section id="about" className="flex pt-16">
-      <div className="w-1/3  flex flex-wrap   text-white ">
-        <h1 className="font-Handy2 h-6 w-auto pl-80 pt-6 rotate-[13deg] text-2xl">
-          My Skills
-        </h1>
-        <img
-          src="/down2.png"
-          className="h-12 ml-80  -mt-32 animate-pulse w-16"
-          alt="logo"
-        />
-        <LanguageComponent languages={languageData} />
-      </div>
-      <div className="w-1/3 p-16 text-white">
-        <span className="font-Handy2 flex">
-          <h1>ME!!</h1>
-          <img src="/down3.png" className="h-12 animate-pulse w-12" />
-        </span>
-        <img
-          className="h-[420px] w-auto pr-12 rounded-full  blur-sm hover:blur-0 hover:"
-          src="/mipic.jpg"
-          alt="logo"
-        />
-      </div>
-      <div className="w-1/3 text-white ">
-        <h1 className=" pt-16  text-2xl font-Handy2 -ml-12">
-          {" "}
-          My qualifications
-        </h1>
-        <img src="/down3.png" alt="logo" className="h-12 animate-pulse w-12" />
-        <div className="text-white font-Anton">
-          <span>
-            <h1 className="  underline font-extrabold text-2xl">
-              BACHELOR OF TECHNOLOGY MECH.E.
-            </h1>
-            <h1 className="text-md text-center mr-48">KNIT, SULTANPUR</h1>
-            <h1 className="text-md text-center mr-48 -mt-1">2022-CURRENT</h1>
+    <section id="about" className="pt-16 pb-16 md:pb-24">
+      <div className="container mx-auto flex flex-col md:flex-row items-center">
+        <div className="w-full md:w-1/3 flex flex-wrap text-white justify-center">
+          <h1 className="font-Handy2 h-6 w-auto md:pl-80  pt-6 rotate-[13deg] text-2xl">
+            My Skills
+          </h1>
+          <img
+            src="/down2.png"
+            className="h-12 ml-4 md:ml-80 mt-12 -ml-24 md:mt-16 animate-pulse w-16"
+            alt="logo"
+          />
+          <LanguageComponent languages={languageData} />
+        </div>
+        <div className="hidden md:flex w-full md:w-1/3 p-8 md:p-16 text-white flex-col items-center">
+          <span className="font-Handy2 flex">
+            <h1>ME!!</h1>
+            <img
+              src="/down3.png"
+              className="h-12 animate-pulse w-12"
+              alt="logo"
+            />
           </span>
-          <span>
-            <h1 className="  underline  pt-10 font-extrabold text-2xl">
-              HIGHER SECONDARY CERTIFICATION
-            </h1>
-            <h1 className="text-md text-center mr-48">SGIC, AURAIYA</h1>
-            <h1 className="text-md text-center mr-48 -mt-1">2020-2021</h1>
-          </span>
-          <span>
-            <h1 className=" pt-10 underline font-extrabold text-2xl">
-              SECONDARY SCHOOL CERTIFICATION
-            </h1>
-            <h1 className="text-md text-center mr-48">SGIC, AURAIYA</h1>
-            <h1 className="text-md text-center mr-48 -mt-1">2018-2019</h1>
-          </span>
-          <span>
-            <h1 className="font-Handy2 rotate-[13deg] text-xl mt-20 pb-6 ml-52 text-white">
+          <img
+            className="h-84 w-auto pr-12 rounded-full  hover:blur-0 mt-4"
+            src="/mipic.jpg"
+            alt="logo"
+          />
+        </div>
+        <div className="hidden md:block w-full md:w-1/3 text-white p-4 md:p-0">
+          <h1 className="pt-16 text-2xl font-Handy2 md:-ml-12">
+            My qualifications
+          </h1>
+          <img
+            src="/down3.png"
+            alt="logo"
+            className="h-12 animate-pulse w-12"
+          />
+          <div className="text-white font-Anton">
+            <span>
+              <h1 className="underline font-extrabold text-2xl">
+                BACHELOR OF TECHNOLOGY MECH.E.
+              </h1>
+              <h1 className="text-md text-center md:mr-48">KNIT, SULTANPUR</h1>
+              <h1 className="text-md text-center md:mr-48 -mt-1">
+                2022-CURRENT
+              </h1>
+            </span>
+            <span>
+              <h1 className="underline pt-10 font-extrabold text-2xl">
+                HIGHER SECONDARY CERTIFICATION
+              </h1>
+              <h1 className="text-md text-center md:mr-48">SGIC, AURAIYA</h1>
+              <h1 className="text-md text-center md:mr-48 -mt-1">2020-2021</h1>
+            </span>
+            <span>
+              <h1 className="pt-10 underline font-extrabold text-2xl">
+                SECONDARY SCHOOL CERTIFICATION
+              </h1>
+              <h1 className="text-md text-center md:mr-48">SGIC, AURAIYA</h1>
+              <h1 className="text-md text-center md:mr-48 -mt-1">2018-2019</h1>
+            </span>
+          </div>
+          <span className="mt-10">
+            <h1 className="font-Handy2 rotate-[13deg] text-xl pb-6 md:ml-48 text-center md:text-left">
               My hobby
             </h1>
             <img
               src="/down2.png"
-              className="h-12 animate-pulse ml-52 -mt-12 w-16"
+              className="h-12 animate-pulse mx-auto md:ml-52 -mt-12 w-16"
               alt="logo"
             />
-            <h1 className="font-Handy2 font-extrabold text-3xl pb-2 text-white">
+            <h1 className="font-Handy2 font-extrabold text-3xl pb-2 text-center md:text-left">
               Playing Volleyball
             </h1>
           </span>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const LanguageComponent = ({ languages }) => {
-    return (
-      <div className="h-[70px] p-12 mx-8 items-center justify-center flex flex-wrap w-3/4 -mt-80">
-        {languages.map((language, index) => (
-          <div className="h-[70px] p-4 bg-white bg-opacity-5 " key={index}>
-            <img
-              className="h-8 w-8 transition-transform transform hover:scale-150 "
-              src={language.logo}
-              alt={`${language.name} LOGO`}
-            />
-            <p className=" font-Anton hover:text-purple-900">{language.name}</p>
-          </div>
-        ))}
-      </div>
-    );
+  return (
+    <div className="p-4 mx-2 bg-white bg-opacity-5 items-center justify-center flex flex-wrap w-full md:w-3/4">
+      {languages.map((language, index) => (
+        <div className="h-20 p-4   m-2" key={index}>
+          <img
+            className="h-8 w-8 transition-transform transform hover:scale-150"
+            src={language.logo}
+            alt={`${language.name} LOGO`}
+          />
+          {/* <p className="font-Anton text-white hover:text-purple-900">
+            {language.name}
+          </p> */}
+        </div>
+      ))}
+    </div>
+  );
 };
- 
 
 export default LanguageComponent;
